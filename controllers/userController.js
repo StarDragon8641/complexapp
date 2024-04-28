eda19d6b41e6c40a08f59971fd1f3c36c264af34
Dennis Murnikow <murnikow@innokraft.de>
2024-04-28 14:04:41 +0200

    Part: 106 (1)

--- controllers/userController.js
+++ controllers/userController.js
@@ -84,6 +84,7 @@ exports.profilePostsScreen = function(req, res) {
   // ask our post model for posts by a certain author id
   Post.findByAuthorId(req.profileUser._id).then(function(posts) {
     res.render('profile', {
+      currentPage: "posts",
       posts: posts,
       profileUsername: req.profileUser.username,
       profileAvatar: req.profileUser.avatar,
@@ -99,8 +100,9 @@ exports.profileFollowersScreen = async function(req, res) {
  try {
   let followers = await Follow.getFollowersById(req.profileUser._id)
   res.render('profile-followers', {
+  currentPage: "followers", 
   followers: followers,
-  profileUsername: req.profileUser.username,
+  profileUsername: req.profileUser.username, 
   profileAvatar: req.profileUser.avatar,
   isFollowing: req.isFollowing,
   isVisitorsProfile: req.isVisitorsProfile 
@@ -109,3 +111,19 @@ exports.profileFollowersScreen = async function(req, res) {
   res.render("404")
  }
 }
+
+exports.profileFollowingScreen = async function(req, res) {
+  try {
+   let following = await Follow.getFollowingById(req.profileUser._id)
+   res.render('profile-following', {
+   currentPage: "following",
+   following: following,
+   profileUsername: req.profileUser.username, 
+   profileAvatar: req.profileUser.avatar,
+   isFollowing: req.isFollowing,
+   isVisitorsProfile: req.isVisitorsProfile 
+  }) 
+  } catch {
+   res.render("404")
+  }
+ }
--- router.js
+++ router.js
@@ -9,11 +9,12 @@ const followController = require('./controllers/followController')
 router.get('/', userController.home)
 router.post('/register', userController.register)
 router.post('/login', userController.login)
-router.post('/logout', userController.logout)     
+router.post('/logout', userController.logout)      
 
 // profile related routes
 router.get('/profile/:username', userController.ifUserExists,userController.sharedProfileData, userController.profilePostsScreen)
-router.get('/profile/:username/followers', userController.ifUserExists,userController.sharedProfileData, userController.profileFollowersScreen)    
+router.get('/profile/:username/followers', userController.ifUserExists,userController.sharedProfileData, userController.profileFollowersScreen)
+router.get('/profile/:username/following', userController.ifUserExists,userController.sharedProfileData, userController.profileFollowingScreen)    
 
 // post related routes 
 router.get('/create-post', userController.mustBeLoggedIn, postController.viewCreateScreen)
