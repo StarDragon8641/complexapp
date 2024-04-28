7c0804c110e0a02a5a2d4071db805de26c61363d
Dennis Murnikow <murnikow@innokraft.de>
2024-04-28 18:05:11 +0200

    Part: 109

--- frontend-js/main.js
+++ frontend-js/main.js
@@ -1,3 +1,7 @@
-import Search from './modules/search' 
+import Search from './modules/search'
+import Chat from './modules/chat' 
 
-new Search()
+if(document.querySelector("#chat-wrapper")) {
+ new Chat()
+}
+if (document.querySelector(".header-search-icon")) {new Search()}
